51b3b550b92add321c3310eb6117b8ee3212a79d
Andrew Valentini <95447370+AndrewValentini@users.noreply.github.com>
2022-08-01 13:44:13 -0500

    Update makeFFT.m

--- makeFFT.m
+++ makeFFT.m
@@ -67,6 +67,3 @@ title('Monitor FFT')
 xlabel('Frequency (Hz)')
 ylabel('Amplitude')
 xlim([f1 f2])
-
-%This is an example text for testing on GitHub
-% Making second commit through GitHub to see if MATLAB is able to pull changes
